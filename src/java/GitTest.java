704b6ffbc5a965a682f817cb9047d7b70c46fc32
fu <109525477+fw110120@users.noreply.github.com>
2023-04-11 19:03:27 +0800

    pull commit

--- src/java/GitTest.java
+++ src/java/GitTest.java
@@ -8,7 +8,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("mater test!");
         System.out.println("hot-fix test!");
-
-
+        System.out.println("pull test!");
     }
 }
